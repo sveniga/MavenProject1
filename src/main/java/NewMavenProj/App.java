177a9c7237411a8babda55e7c03a6b576490e393
root <root@ip-172-31-35-138.ec2.internal>
2019-06-08 20:29:43 +0000

    Maven commit one

---
+++ src/main/java/NewMavenProj/App.java
@@ -0,0 +1,13 @@
+package NewMavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
